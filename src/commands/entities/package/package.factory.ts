a37ec5bb97b2aa8bff69a75b8c0709b33d97bb52
LeeShinYeoung <iqrash12@gmail.com>
2023-02-20 00:25:26 +0900

    Configure the package dependency

--- src/commands/entities/package/package.factory.ts
+++ src/commands/entities/package/package.factory.ts
@@ -18,12 +18,13 @@ export class packageFactory {
     properties.version = '1.0.0'
     properties.description = packageDescription || ''
     properties.scripts = this.getScripts(packageAddition)
+    properties.dependencies = this.getDependencies(packageAddition)
 
     return new Package(properties)
   }
 
   private static getScripts(packageAddition?: string[]) {
-    const scripts: { [key: string]: string } = {
+    const scripts: PackageProperties['scripts'] = {
       start: PackageScripts.start,
       build: PackageScripts.build
     }
@@ -38,6 +39,32 @@ export class packageFactory {
 
     return scripts
   }
+
+  private static getDependencies(packageAddition?: string[]) {
+    const dependencies: PackageProperties['dependencies'] = {}
+
+    dependencies['nodemon'] = PackageDependencies['nodemon']
+    dependencies['ts-node'] = PackageDependencies['ts-node']
+    dependencies['typescript'] = PackageDependencies['typescript']
+
+    if (packageAddition?.includes('prettier')) {
+      dependencies['prettier'] = PackageDependencies['prettier']
+    }
+
+    if (packageAddition?.includes('eslint')) {
+      dependencies['@typescript-eslint/eslint-plugin'] =
+        PackageDependencies['@typescript-eslint/eslint-plugin']
+      dependencies['@typescript-eslint/parser'] =
+        PackageDependencies['@typescript-eslint/parser']
+      dependencies['eslint'] = PackageDependencies['eslint']
+      dependencies['eslint-config-prettier'] =
+        PackageDependencies['eslint-config-prettier']
+      dependencies['eslint-plugin-prettier'] =
+        PackageDependencies['eslint-plugin-prettier']
+    }
+
+    return dependencies
+  }
 }
 
 export const PackageScripts = {
@@ -47,4 +74,14 @@ export const PackageScripts = {
   prettier: "prettier --config .prettierrc 'src/**/*.ts' --write"
 }
 
-export const PackageDependencies = {}
+export const PackageDependencies = {
+  nodemon: '^2.0.20',
+  'ts-node': '^10.9.1',
+  typescript: '^4.8.4',
+  '@typescript-eslint/eslint-plugin': '^5.40.0',
+  '@typescript-eslint/parser': '^5.40.0',
+  eslint: '^8.25.0',
+  'eslint-config-prettier': '^8.5.0',
+  'eslint-plugin-prettier': '^4.2.1',
+  prettier: '^2.7.1'
+}
