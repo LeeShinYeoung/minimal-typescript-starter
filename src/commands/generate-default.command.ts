f76297f836776dc6b36f883dbf9ca3aa45579144
LeeShinYeoung <iqrash12@gmail.com>
2023-02-19 23:20:18 +0900

    Implement a generator that produces results

---
+++ src/commands/generate-default.command.ts
@@ -0,0 +1,21 @@
+import { Answers } from 'prompts'
+import { FileMananger } from '../libs/file-manager'
+import { packageFactory } from './entities/package/package.factory'
+
+export interface GenerateDefaultCommand {
+  execute(response: Answers<string>): void
+}
+
+export class GenerateDefaultCommandImpl implements GenerateDefaultCommand {
+  constructor(private readonly fileManager: FileMananger) {}
+
+  async execute(response: Answers<string>) {
+    const { packageName, packageDescription } = response
+
+    const packageEntity = packageFactory.create({
+      packageName,
+      packageDescription
+    })
+    this.fileManager.saveFile('package.json', packageEntity.toString())
+  }
+}
