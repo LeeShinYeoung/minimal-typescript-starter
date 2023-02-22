679cc4e50bb38bcd395b664a77bddf240421db0c
LeeShinYeoung <iqrash12@gmail.com>
2023-02-23 02:03:13 +0900

    Add npm-manager

---
+++ src/libs/npm-manager.ts
@@ -0,0 +1,37 @@
+import { spawn } from 'child_process'
+
+export interface NpmManager {
+  install(packages?: string[]): void
+}
+
+export class NpmManagerImpl {
+  constructor(private readonly path: string) {}
+
+  async install(packages?: string[]) {
+    const path = process.env.PWD + '/' + this.path
+
+    await new Promise((resolve, reject) => {
+      const command = spawn('npm', ['install', ...(packages || [])], {
+        stdio: 'inherit',
+        cwd: path
+      })
+
+      command?.stdout?.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+      })
+
+      command?.stderr?.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+      })
+
+      command.on('close', (code) => {
+        if (code !== 0) {
+          console.error(`npm install failed`)
+          reject()
+        } else {
+          resolve(null)
+        }
+      })
+    })
+  }
+}
