54c783cc6b00166216b880456d635ef5cde89faf
LeeShinYeoung <iqrash12@gmail.com>
2023-02-23 02:06:25 +0900

    Attach npm manager to index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+#!/usr/bin/env node
 import { FileManangerImpl } from './libs/file-manager'
 import { GenerateDefaultCommandImpl } from './commands/generate-default.command'
 import { PromptController } from './controllers/prompt.controller'
+import { NpmManagerImpl } from './libs/npm-manager'
 
-const fileManager = new FileManangerImpl()
-const generateDefaultCommand = new GenerateDefaultCommandImpl(fileManager)
+const fileManager = new FileManangerImpl(process.argv[2] || '.')
+const npmManager = new NpmManagerImpl(process.argv[2] || '.')
+const generateDefaultCommand = new GenerateDefaultCommandImpl(
+  fileManager,
+  npmManager
+)
 const promptController = new PromptController(generateDefaultCommand)
 
 promptController
@@ -12,5 +18,5 @@ promptController
     return promptController.control(response)
   })
   .catch((error) => {
-    console.error(error)
+    console.error(error.message)
   })
