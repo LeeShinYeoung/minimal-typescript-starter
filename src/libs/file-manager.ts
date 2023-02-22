2a0820753e6acccfbd2485663497e602444657cd
LeeShinYeoung <iqrash12@gmail.com>
2023-02-23 02:03:44 +0900

    Refactor file-manager

--- src/libs/file-manager.ts
+++ src/libs/file-manager.ts
@@ -2,15 +2,26 @@ import * as fs from 'fs'
 import { promisify } from 'util'
 
 export interface FileMananger {
+  checkDirectory(): void
   saveFile(filename: string, content: string): void
 }
 
 export class FileManangerImpl implements FileMananger {
-  async saveFile(filename: string, content: string) {
-    if (!fs.existsSync('temp')) {
-      fs.mkdirSync('temp')
+  constructor(private readonly path: string) {}
+
+  checkDirectory() {
+    const path = process.env.PWD + '/' + this.path
+
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path)
     }
 
-    await promisify(fs.writeFile)(`temp/${filename}`, content)
+    if (fs.readdirSync(path).length) {
+      throw new Error('Directory is not empty')
+    }
+  }
+
+  async saveFile(filename: string, content: string) {
+    await promisify(fs.writeFile)(`${this.path}/${filename}`, content)
   }
 }
